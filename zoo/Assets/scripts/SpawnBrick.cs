4633081e53d8cecaefe48a56d97dd3df48c336ab
KaisaNorak <kaisa.norak@gmail.com>
2020-11-15 23:53:19 +0200

    Made bricks appear, fall and disappear

---
+++ zoo/Assets/scripts/SpawnBrick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBrick : MonoBehaviour
+{
+    public GameObject brickPrefab;
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(brickWave());
+    }
+
+    private void spawnEnemy()
+    {
+        GameObject b = Instantiate(brickPrefab) as GameObject;
+        b.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y);
+        b.transform.Rotate(Vector3.forward * Random.Range(90, -90));
+    }
+
+    IEnumerator brickWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+        }
+        
+    }
+}
