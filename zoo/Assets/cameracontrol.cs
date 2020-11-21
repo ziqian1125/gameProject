54f96b1a25d111123c3c187079ea0f222a4e8639
ziqian1125 <71480117+ziqian1125@users.noreply.github.com>
2020-11-21 23:29:58 +0800

    add camera follow player

---
+++ zoo/Assets/cameracontrol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontrol : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    public Transform followObject;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(followObject.position.x,followObject.position.y,-10f);
+
+    }
+}
