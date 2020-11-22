89c61e2f0830d144c34b7b058b6cbde21aa4d49a
ziqian1125 <71480117+ziqian1125@users.noreply.github.com>
2020-11-22 22:21:29 +0800

    add StaminaControl(for run)

---
+++ zoo/Assets/scripts/staminaControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class staminaControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int staminaNum;
+    public Text stamina; 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            // print("hello world");
+            print(stamina.text.GetType());
+            staminaNum=int.Parse(stamina.text)-1;
+            stamina.text = staminaNum.ToString();
+
+
+            //do stuff
+        }       
+        
+    }
+}
