a3e855028bb8b53995c04e16efc96a1d3a57f977
ziqian1125 <71480117+ziqian1125@users.noreply.github.com>
2020-11-22 15:38:13 +0800

    collect food

---
+++ zoo/Assets/scripts/collectfood.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectfood : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Cherry = 1;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Collection")
+        {
+            Destroy(collision.gameObject);
+            Cherry +=1;
+        }
+        
+    } 
+}
