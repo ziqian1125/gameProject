e18eff0747481a04e744c5d7a18a9f22c981a654
ziqian1125 <71480117+ziqian1125@users.noreply.github.com>
2020-11-23 00:01:45 +0800

    add entertain drops to 0 logic

---
+++ zoo/Assets/scripts/entertainControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class entertainControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text entertain; 
+    private int entertainNum;
+
+
+    void Start()
+    {
+        entertain.text = "3";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (entertain.text=="0")
+            {
+            print("catch"); 
+            }
+
+        if (Input.GetKeyUp(KeyCode.C))
+            {
+                entertainNum=int.Parse(entertain.text)-1;
+                entertain.text = entertainNum.ToString();
+            }  
+        
+        
+    }
+}
