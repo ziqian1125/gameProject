c7a538c97b898121edba0b8c8d3e06cbdd7d5334
ziqian1125 <71480117+ziqian1125@users.noreply.github.com>
2020-11-22 23:44:37 +0800

    add health drops to 0 logic

---
+++ zoo/Assets/scripts/healthControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class healthControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text health;
+    private int healthNum;
+ 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (health.text=="0")
+        {
+           print("die"); 
+        }
+
+    if (Input.GetKeyUp(KeyCode.V))
+        {
+            healthNum=int.Parse(health.text)-1;
+            health.text = healthNum.ToString();
+        }  
+    }
+
+ 
+}
