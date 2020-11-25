cd739f0140bc534358e061bd54cf19ce0e0741cc
ziqian1125 <71480117+ziqian1125@users.noreply.github.com>
2020-11-25 23:00:24 +0800

    merge jump and run actions into main

--- zoo/Assets/scripts/PlayerMovement.cs
+++ zoo/Assets/scripts/PlayerMovement.cs
@@ -6,55 +6,111 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator charAnim;
-    public float moveSpeed;
-   // public float runSpeed;
-    public float jumpSpeed;
+    [SerializeField] private float moveSpeed;
+    public float runSpeed;
+    [SerializeField] private float jumpSpeed;
+    // idle = 0, walking = 1, jumping = 2 , etc
+    private enum State {idle, walking, jumping, falling, running};
+    private State animState = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         charAnim = GetComponent<Animator>();
-        
+        coll = GetComponent<Collider2D>();
+
+        moveSpeed = 2f;
+        runSpeed = 4f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+        AnimationState();
+        charAnim.SetInteger("state", (int)animState);
+    }
 
+    private void AnimationState()
+    {
+        if(animState == State.jumping)
+        {
+            if(rb.velocity.y < -0.5f)
+            {
+                animState = State.falling;
+            }
+        }
+
+      else if (animState == State.falling)
+        {
+            if(coll.IsTouchingLayers(ground))
+            {
+                animState = State.idle;
+            }
+        }
+       
+        else if (Mathf.Abs(rb.velocity.x) > 1.8f)
+        {
+            if (Mathf.Abs(rb.velocity.x) > 3f)
+             {
+                 //running
+                 animState = State.running;
+             }
+
+             else
+             {
+            //moving
+            animState = State.walking;
+          }
+        }
+
+      
+
+        else
+        {
+            animState = State.idle;
+        }
+
+    }   
+
+    private void Movement()
+    {
         //Player Input for for movement
         float horDir = Input.GetAxis("Horizontal");
-
+ 
+        //direction
         if (horDir < 0)
-        {
+        { //left
             rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
-            transform.localScale = new Vector2(-0.5f, 0.5f);
-            charAnim.SetBool("walking", true);
+            transform.localScale = new Vector2(-0.4f, 0.4f);
         }
 
         else if (horDir > 0)
         {
+            //right
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
-            transform.localScale = new Vector2(0.5f, 0.5f);
-            charAnim.SetBool("walking", true);
+            transform.localScale = new Vector2(0.4f, 0.4f);
+        }
+
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            animState = State.jumping;
+        }
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = runSpeed;
         }
 
         else
         {
-            charAnim.SetBool("walking", false);
+            moveSpeed = 2f;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-
-        }
-
-        /*while (Input.GetKey(KeyCode.LeftShift))
-        {
-            moveSpeed = runSpeed;
-        }*/
-        
-
+      
     }
 }
