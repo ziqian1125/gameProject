2d98e86ab482d468c10d8a62c7f90c18613d8b6b
ziqian1125 <71480117+ziqian1125@users.noreply.github.com>
2020-11-26 22:17:03 +0800

    when jump on traps,health drops

---
+++ zoo/Assets/garbage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class garbage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.tag == "garbage"){
+            print("l jump on garbage");
+        }
+    } 
+
+}
