0ddba32f7e50ea167deca85283d4ed10667c6e5c
kkr9551 <71511316+kkr9551@users.noreply.github.com>
2020-11-26 12:06:48 +0800

    Add files via upload
    i have coded main menu, so game can be paused, and main menu appears when player press the escape key. In main menu, there are three buttons: resume,  restart, and quit. Pressing restart button and quit button triggers the confirm menu respectively. 
    Also, I have coded play menu. Players start game through the play button

---
+++ confirmMenu_2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class confirmMenu_2 : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
