3ac3a24797c19c8ff3368f9e0b96ded8ad21b8d5
Yogini Mauree <yosoykinidesign@gmail.com>
2020-12-04 17:05:03 +0400

    Update Script
    Added comments

--- zoo/Assets/scripts/SuccessPlayerMove.cs
+++ zoo/Assets/scripts/SuccessPlayerMove.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Yogini's Script
 public class SuccessPlayerMove : MonoBehaviour
 {
+    // declaring variable
     private Rigidbody2D rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-       
-    rb = GetComponent<Rigidbody2D>();
-}
+        // accessing rigidbody component   
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        //having the player go up the rope in the "winning" scene
         rb.velocity = transform.up * 2f;
     }
 }
